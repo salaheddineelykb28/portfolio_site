5b70450853f6a31f5a4539e5e7f99f135b21e34c
salaheddineelykb28 <salaheddineelyaakouby28@gmail.com>
2025-01-29 19:27:21 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    setTimeout(function () {
+        document.getElementById('loading').classList.add('hidden'); // Masquer avec une animation de fondu
+    }, 1000);  // Attendre 1 seconde après le chargement pour cacher l'animation
+};
